c8519387b5ff98e43c476b5d763133cf276c597b
Lucas Cruz dos Reis <lcr.ergo@gmail.com>
2019-09-08 09:59:03 -0300

    Changing colorscheme configuration

--- config.def.h
+++ config.def.h
@@ -8,10 +8,10 @@ static const char *fonts[] = {
 };
 static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
 static const char *colors[SchemeLast][2] = {
-	/*     fg         bg       */
-	[SchemeNorm] = { "#bbbbbb", "#222222" },
-	[SchemeSel] = { "#eeeeee", "#bf616a" },
-	[SchemeOut] = { "#000000", "#990000" },
+                        /*     fg         bg       */
+	[SchemeNorm]    = { "#bbbbbb", "#111111" },
+	[SchemeSel]     = { "#eeeeee", "#990000" },
+	[SchemeOut]     = { "#000000", "#00ffff" },
 };
 /* -l option; if nonzero, dmenu uses vertical list with given number of lines */
 static unsigned int lines      = 0;
